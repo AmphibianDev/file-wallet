5f6f106acc53426ea0ce6572dc3c49e8d3343fb4
AmphibianDev <110111354+AmphibianDev@users.noreply.github.com>
2023-08-08 21:04:29 +0300

    Replace Twitter svg with X svg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { AiFillGithub, AiOutlineTwitter } from 'react-icons/ai';
+import { AiFillGithub } from 'react-icons/ai';
 
 import MiddleTruncate from './MiddleTruncate';
 
@@ -25,12 +25,13 @@ const Footer = () => {
         </p>
       </div>
       <div className={FooterCSS.icons}>
-        <a
-          href="https://twitter.com/AmphibianDev"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <AiOutlineTwitter />
+        <a href="https://x.com/AmphibianDev" target="_blank" rel="noreferrer">
+          {/* Taken from 𝕏 directly until is available on react-icons*/}
+          <svg viewBox="0 0 24 24">
+            <g>
+              <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
+            </g>
+          </svg>
         </a>
         <a
           href="https://github.com/AmphibianDev/file-wallet"
