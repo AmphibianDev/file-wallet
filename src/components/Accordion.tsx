da7b5281304de9d6de4257b495c9c746e2cde486
AmphibianDev <110111354+AmphibianDev@users.noreply.github.com>
2023-09-14 21:32:44 +0300

    Refactor Accordion by removing Headless UI

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,23 +1,55 @@
-import { ReactNode } from 'react';
-import { Disclosure } from '@headlessui/react';
+import React, { useState, useRef, useEffect } from 'react';
 import AccordionCSS from './Accordion.module.css';
 
 type Props = {
   title: string;
-  children: ReactNode;
+  children: React.ReactNode;
 };
 
 const Accordion = ({ title, children }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef<HTMLDivElement | null>(null);
+
+  // remove tab focus from content when accordion is closed
+  useEffect(() => {
+    const focusableElements = contentRef.current?.querySelectorAll(
+      'a, button, input, [tabindex]:not([tabindex="-1"])'
+    );
+
+    if (focusableElements) {
+      Array.from(focusableElements).forEach(el => {
+        if (isOpen) {
+          el.setAttribute('tabindex', '0');
+        } else {
+          el.setAttribute('tabindex', '-1');
+        }
+      });
+    }
+  }, [isOpen]);
+
   return (
-    <Disclosure as="div" className={AccordionCSS.container}>
-      <Disclosure.Button className={AccordionCSS.header}>
+    <div className={AccordionCSS.container} role="region">
+      <button
+        role="button"
+        id="accordion-header"
+        aria-controls="accordion-content"
+        aria-expanded={isOpen}
+        onClick={() => setIsOpen(!isOpen)}
+        className={AccordionCSS.header}
+      >
         <h2>{title}</h2>
         <div className="x-btn"></div>
-      </Disclosure.Button>
-      <Disclosure.Panel unmount={false} className={AccordionCSS.content}>
+      </button>
+      <div
+        ref={contentRef}
+        id="accordion-content"
+        aria-labelledby="accordion-header"
+        hidden={!isOpen}
+        className={AccordionCSS.content}
+      >
         <div>{children}</div>
-      </Disclosure.Panel>
-    </Disclosure>
+      </div>
+    </div>
   );
 };
 
