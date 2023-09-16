fb0f08726dba04d129ba7fe7f71bc92ae546850a
AmphibianDev <110111354+AmphibianDev@users.noreply.github.com>
2023-09-16 19:48:04 +0300

    Add: BIP39 JS

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect, useRef } from 'react';
+
 import { Routes, Route } from 'react-router-dom';
 
 import NavBar from './components/NavBar';
@@ -10,6 +12,71 @@ import QRCodeModal from './components/QRCodeModal';
 import AppCSS from './App.module.css';
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const scriptsLoadedRef = useRef(false); // for stopping running twice because of React.StrictMode
+
+  useEffect(() => {
+    if (scriptsLoadedRef.current) return;
+    scriptsLoadedRef.current = true;
+
+    // function to load a single script
+    const loadScript = (src: string) => {
+      return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.onload = resolve;
+        script.onerror = reject;
+        document.body.appendChild(script);
+      });
+    };
+
+    // list of scripts to load
+    const scripts = [
+      'bip39/bip39-libs.js',
+      'bip39/bitcoinjs-extensions.js',
+      'bip39/segwit-parameters.js',
+      'bip39/ripple-util.js',
+      'bip39/jingtum-util.js',
+      'bip39/casinocoin-util.js',
+      'bip39/cosmos-util.js',
+      'bip39/eos-util.js',
+      'bip39/fio-util.js',
+      'bip39/xwc-util.js',
+      'bip39/sjcl-bip39.js',
+      'bip39/wordlist_english.js',
+      'bip39/wordlist_japanese.js',
+      'bip39/wordlist_spanish.js',
+      'bip39/wordlist_chinese_simplified.js',
+      'bip39/wordlist_chinese_traditional.js',
+      'bip39/wordlist_french.js',
+      'bip39/wordlist_italian.js',
+      'bip39/wordlist_korean.js',
+      'bip39/wordlist_czech.js',
+      'bip39/wordlist_portuguese.js',
+      'bip39/jsbip39.js',
+      'bip39/entropy.js',
+      'bip39/index.js',
+    ];
+
+    // function to load all scripts
+    const loadAllScripts = async () => {
+      for (const src of scripts) {
+        // console.log("loading script", src);
+        await loadScript(src);
+      }
+      setIsLoaded(true);
+    };
+
+    // start loading all scripts
+    loadAllScripts().catch(error => {
+      console.error('Error loading scripts:', error);
+    });
+  }, []);
+
+  if (!isLoaded) {
+    return <div>Loading...</div>; // Your loading screen here
+  }
+
   return (
     <div className={AppCSS.container}>
       <InfoPopup />
---
+++ src/bip39.d.ts
@@ -0,0 +1,28 @@
+interface AddressInfo {
+  derivationPath: string;
+  address: string;
+  privkey: string;
+  pubkey: string;
+}
+
+interface Bip39Info {
+  errorMessage: string;
+  bip39Mnemonic: string;
+  bip39Seed: string;
+  bip39RootKey: string;
+  accountExtendedPrvKey: string;
+  accountExtendedPubKey: string;
+  derivationPath: string;
+  wallets: AddressInfo[];
+}
+
+interface Window {
+  networksNames: string[];
+  setCrypto: (cryptoName: string) => boolean;
+  sha256: (message: string) => Promise<string>;
+  getMnemonicFromEntropy: (entropyStr: string) => string;
+  generateFromMnemonic: (
+    mnemonic: string,
+    numberOfWallets?: number
+  ) => Bip39Info;
+}
