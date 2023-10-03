792dbd4d89668fb1c8678a3808cf6b566284b252
AmphibianDev <110111354+AmphibianDev@users.noreply.github.com>
2023-10-03 18:29:20 +0300

    Update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,38 +1,54 @@
 import { AiFillGithub } from 'react-icons/ai';
+import { RiTwitterXLine } from 'react-icons/ri';
 
 import MiddleTruncate from './MiddleTruncate';
+
+import useInfoPopupStore from './InfoPopup.store';
 
 import FooterCSS from './Footer.module.css';
 
 const Footer = () => {
+  const { openPopup } = useInfoPopupStore();
+
+  const myMoneroAddress =
+    '49FuwMjQZBHjnvdpk2hxYWNy !! THIS IS A PLACE HOLDER !! YsbSPETdfPubVoZQcbPpgPPbN3QqamV4';
+
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(myMoneroAddress)
+      .then(() => {
+        openPopup('copied', 'success', 1000);
+      })
+      .catch(error => {
+        console.error('Failed to copy text: ', error);
+        openPopup('failed to copy', 'error', 1000);
+      });
+  };
+
   return (
     <footer className={FooterCSS.container}>
       <div className={FooterCSS.text}>
         <p>
           Donate <u>Monero</u>:{' '}
-          <button className={FooterCSS.clickText}>
-            <MiddleTruncate
-              minChars={7}
-              maxChars={7}
-              text="49FuwMjQZBHjnvdpk2hxYWNyufQ9Js5RrAwqq6qs8muaZima8iZzQuuC3jNfjCcYsbSPETdfPubVoZQcbPpgPPbN3QqamV4"
-            />
+          <button className={FooterCSS.clickText} onClick={handleCopy}>
+            <MiddleTruncate minChars={7} maxChars={7} text={myMoneroAddress} />
           </button>
         </p>
         <p>
           App Version:{' '}
-          <a href="" className={FooterCSS.clickText}>
+          <a
+            href="https://github.com/AmphibianDev/file-wallet"
+            target="_blank"
+            rel="noreferrer"
+            className={FooterCSS.clickText}
+          >
             0.1.1V
           </a>
         </p>
       </div>
       <div className={FooterCSS.icons}>
         <a href="https://x.com/AmphibianDev" target="_blank" rel="noreferrer">
-          {/* Taken from 𝕏 directly until is available on react-icons*/}
-          <svg viewBox="0 0 24 24">
-            <g>
-              <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
-            </g>
-          </svg>
+          <RiTwitterXLine />
         </a>
         <a
           href="https://github.com/AmphibianDev/file-wallet"
