a1edeeb02e5020057534b833c3e63593638cf11b
AmphibianDev <110111354+AmphibianDev@users.noreply.github.com>
2023-11-20 23:57:40 +0200

    Add action for GitHub Pages and un-minify for better transparency

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: 'https://file-wallet.com/',
+  build: {
+    minify: false, // Disable minification in production build for easier code review
+  },
+});
