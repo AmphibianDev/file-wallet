47939b00431d2ed84f4aa7b01249da48dc6d735d
AmphibianDev <110111354+AmphibianDev@users.noreply.github.com>
2023-12-11 17:02:43 +0200

    fix: 404 on FAQ page refresh and Google indexing

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 import './reset.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
